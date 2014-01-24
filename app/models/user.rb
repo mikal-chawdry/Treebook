bc79033245a4ea44532a59bd9b9d2b781b5dbe52
Mikal Chawdry <mikalchawdry@gmail.com>
2014-01-24 11:22:15 +0000

    Added tests for user first name, last name and profile correctness

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,16 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
   
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, 
+                          uniqueness: true,
+                          format: {
+                            with: /a-zA-Z0-9_-/,
+                            message: 'must be formatted correctly'
+                          }
+                          
+                          
   has_many :status # We now have the method status available to instances of the User class. If we call @user.statuses it will return an array of all the status objects. 
   
   def full_name
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,42 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "a user should have to enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty? # We want an error to be raised if the user does not enter a first name. When an error occurs, it gets passed to the error object making it not empty. If it is empty, this implies that when a user does not enter their first name, no error is raised. To make test pass, you have to use validation methods in the user model. The errors object is an array
+  end
+  
+  test "a user should have to enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+  
+  test "a user should have to enter a profile name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+  end
+  
+  test "user should have a unique profile name" do
+    user = User.new
+    user.profile_name = users(:mikal).profile_name
+    users(:mikal)
+    user.password = "password"
+    user.password_confirmation = "password"
+    assert !user.save
+    puts user.errors.inspect
+    assert !user.errors[:profile_name].empty?
+  end
+  
+  test "A user should have a profile name without any spaces" do
+    user = User.new
+    user.profile_name = "mikal chawdry"
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert user.errors[:profile_name].include?("must be formatted correctly")
+  end
+  
 end
