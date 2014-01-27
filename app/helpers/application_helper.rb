c4fd35aa899e13826be6ec0d54044af261e94e30
Mikal Chawdry <mikalchawdry@gmail.com>
2014-01-27 16:50:34 +0000

    styled the flash messages and added a global helper method to help with this

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def flash_class(type)
+    if type == :notice
+      "alert-success"
+    elsif type == :alert
+      "alert-danger"
+    else
+      ""
+    end
+  end
 end
